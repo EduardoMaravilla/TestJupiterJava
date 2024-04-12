23f7f956e6594a24ebf87eea003d46f02311faf8
Eduardo Maravilla <eduardomaravillag@gmail.com>
2024-04-11 19:36:28 -0600

    Update

--- testbasic/src/test/java/org/eduardomaravill/classes/SumTest.java
+++ testbasic/src/test/java/org/eduardomaravill/classes/SumTest.java
@@ -17,36 +17,31 @@ class SumTest {
     void operationSumInt() {
         int a = 10;
         int b = 10;
-        int c = operation.operationSum(a,b);
-        assertEquals(20, c);
-        assertInstanceOf(Integer.class,c);
+        assertEquals(20, operation.operationSum(a,b));
+        assertInstanceOf(Integer.class,operation.operationSum(a,b));
     }
 
     @Test
     void testOperationSumDouble() {
         double a = 10.5;
         double b = 10.5;
-        double c = operation.operationSum(a, b);
-        assertEquals(21.0, c);
-        assertInstanceOf(Double.class,c);
+        assertEquals(21.0, operation.operationSum(a,b));
+        assertInstanceOf(Double.class,operation.operationSum(a,b));
     }
 
     @Test
     void testOperationSumFloat() {
         float a = 10.5f;
         float b = 10.5f;
-        float c = operation.operationSum(a, b);
-        assertEquals(21.0f, c);
-        assertInstanceOf(Float.class,c);
+        assertEquals(21.0f, operation.operationSum(a,b));
+        assertInstanceOf(Float.class,operation.operationSum(a,b));
     }
 
     @Test
     void testOperationSumLong() {
         long a = 10L;
         long b = 10L;
-        long c = operation.operationSum(a, b);
-        assertEquals(20L, c);
-        assertInstanceOf(Long.class,c);
+        assertEquals(20L, operation.operationSum(a,b));
+        assertInstanceOf(Long.class,operation.operationSum(a,b));
     }
-
 }
